2cdffcf03a1af31bb992169852fa4d30a0a42471
tayfundagci <tayfundagci3517@gmail.com>
2023-10-25 15:15:56 +0300

    test

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const index = () => {
   return (
-    <div>About</div>
+    <div className='flex justify-center px-4 md:px-0'>
+      <a href="#" className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-[#212121] dark:border-gray-700 hover:dark:bg-[#191919]">
+        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+        <p className="font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+      </a>
+    </div>
   )
 }
 
--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className='flex justify-center'>
-      <a href="#" className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-[#212121] dark:border-gray-700 hover:dark:bg-[#191919]">
-        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-        <p className="font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-      </a>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Omnis corrupti ipsa ipsam fugiat neque aperiam esse exercitationem non aut! Consequatur recusandae autem ullam sint libero blanditiis aut facilis aspernatur porro.
     </main>
   )
 }
